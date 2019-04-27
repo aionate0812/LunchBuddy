d59611cb95ab452b5a0d6a808aaeaad73f84bd0b
Heriberto Uroza <heribertouroza@pursuit.org>
2019-04-27 18:38:12 -0400

    created routes, need to work on dbconnect

--- lunch-buddy-backend/services/user.js
+++ lunch-buddy-backend/services/user.js
@@ -1,17 +1,13 @@
-const db = require('./dbConnect')
+const {db} = require('./dbConnect')
 
 const UserService = {}
 
 UserService.createUser = (email_or_username) => {
-    db.one('INSERT INTO users (email_or_username) VALUES (${email_or_username}) RETURNING id', { email_or_username })
+    return db.one('INSERT INTO users (email_or_username) VALUES (${email_or_username}) RETURNING id', { email_or_username })
 }
 
-// UserService.getUserbyID = (email_or_username) => {
-//    
-// }
-
-UserService.getUserbyEOU = (email_or_username) => {
-    db.any('SELECT * from users where email_or_username = ${email_or_username}', { email_or_username})
+UserService.getUserbyID = (id) => {
+    return db.any('SELECT * from users where id = ${id}', { id })
 }
 
 module.exports = UserService
