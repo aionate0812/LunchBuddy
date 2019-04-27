b3350a4bc384a17cab31dd0bad4e772186ba09a5
Heriberto Uroza <heribertouroza@pursuit.org>
2019-04-27 15:30:45 -0400

    added front-end UserLogin Page Along with Nav Bar

--- lunch-buddy-frontend/src/components/NavBar.js
+++ lunch-buddy-frontend/src/components/NavBar.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const NavBar = props => {
     return (
         <>
-            <h2>NavBar</h2>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <Link className="navbar-brand" to='/'> Lunch Buddy </Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav">
+                        <li className="nav-item active">
+                            <Link className="nav-link" to="/"> Home </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
 
         </>
     )
