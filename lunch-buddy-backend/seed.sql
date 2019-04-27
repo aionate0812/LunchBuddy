a5f33c50e81f609088b6e1de1345e3a8ee87ab3f
Heriberto Uroza <heribertouroza@pursuit.org>
2019-04-27 13:58:30 -0400

    test files

--- lunch-buddy-backend/seed.sql
+++ lunch-buddy-backend/seed.sql
@@ -3,3 +3,15 @@ CREATE DATABASE lunch_buddy;
 
 \c lunch_buddy;
 
+CREATE TABLE users
+(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR NOT NULL UNIQUE
+);
+
+INSERT INTO users
+    (email)
+VALUES
+    ('test@test.com'),
+    ('test1@test1.com'),
+    ('test2@test2.com')
