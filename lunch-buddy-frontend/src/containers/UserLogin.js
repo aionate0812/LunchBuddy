02d3f9ddc970a6f9faa900993e877c1f3e3406a9
AaronTeele <aaronteele@AaronTeeles-MacBook-Air.local>
2019-04-28 14:05:09 -0400

    added create user feature

--- lunch-buddy-frontend/src/containers/UserLogin.js
+++ lunch-buddy-frontend/src/containers/UserLogin.js
@@ -5,6 +5,8 @@ class UserLogin extends React.Component {
     state = {
         input: '',
         user_id: '',
+        createUsername: '',
+        createEmail: '',
         email: '',
         error: ''
     }
@@ -39,7 +41,34 @@ class UserLogin extends React.Component {
                     error: err.toString()
                 })
             })
-            
+
+    }
+
+    handleCreateSubmit = (e) => {
+        e.preventDefault()
+
+        axios.post('http://localhost:5000/user/create', {
+            username: this.state.createUsername,
+            email: this.state.createEmail
+        })
+            .then(res => {
+                console.log(res)
+                this.setState({
+                    user_id: res.data.data.id,
+                })
+            })
+            .then(() => {
+                console.log(this.state)
+                localStorage.setItem('user', JSON.stringify(this.state))
+            })
+            .then(() => {
+                this.props.history.push('/dashboard')
+            })
+            .catch(err => {
+                this.setState({
+                    error: err.toString()
+                })
+            })
     }
 
     render() {
@@ -49,9 +78,9 @@ class UserLogin extends React.Component {
                 <div className="col-xs-12" style={{ "height": "75px", "textAlign": "center" }}>Please Login to Find Your Lunch Buddy</div>
                 <div className='container my-auto'>
                     <div className='row'>
-                    {
-                     this.state.error   
-                    }
+                        {
+                            this.state.error
+                        }
                         <form style={{ 'width': '100%' }} >
                             <div className="input-group flex-nowrap my-3">
 
@@ -59,6 +88,25 @@ class UserLogin extends React.Component {
                             </div>
                             <div style={{ 'textAlign': 'center' }}>
                                 <button type="button" className="btn btn-primary btn-md" onClick={this.handleSubmit}>Login</button>
+                            </div>
+                        </form>
+                    </div>
+                    <hr />
+                </div>
+                <br />
+                <div className="col-xs-12" style={{ "height": "75px", "textAlign": "center" }}>Or Create An Account</div>
+                <div className='container my-auto'>
+                    <div className='row'>
+                        {
+                            this.state.error
+                        }
+                        <form style={{ 'width': '100%' }} >
+                            <div className="input-group flex-nowrap my-3">
+                                <input type="text" name='createEmail' className="form-control" placeholder="foodLover@example.com" aria-label="Create Email" aria-describedby="addon-wrapping" onChange={this.handleChange} />
+                                <input type="text" name='createUsername' className="form-control" placeholder="Enter Username" aria-label="Create Username" aria-describedby="addon-wrapping" onChange={this.handleChange} />
+                            </div>
+                            <div style={{ 'textAlign': 'center' }}>
+                                <button type="button" className="btn btn-secondary btn-md" onClick={this.handleCreateSubmit}>Create Account</button>
                             </div>
                         </form>
                     </div>
