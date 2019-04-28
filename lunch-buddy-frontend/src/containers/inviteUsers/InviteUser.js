18e2186c36b41b4fd935deeb37f81f9c1c706752
ositaigwe@pursuit.org <j0h@example.com>
2019-04-28 00:49:31 -0400

    added InviteUser.js

--- lunch-buddy-frontend/src/containers/inviteUsers/InviteUser.js
+++ lunch-buddy-frontend/src/containers/inviteUsers/InviteUser.js
@@ -1,74 +1,100 @@
 import React, { Component } from 'react';
-import {Redirect, Link} from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
+import axios from 'axios'
 
 const port = 5000
 
-const read = () =>{
-    return axios ({
-        method: "get",
-        url: `http://localhost:${port}/orders`,
+const createOrderRequest = (order_id, user_id) =>{
+    return axios({
+        method: "posts",
+        url: `http://localhost:${port}/order_request/`,
+        data: {
+            order_id: order_id,
+            user_id: user_id
+        }
     })
 }
 
 
-class RestaurantSearchResults extends React.Component {
+class InviteUsers extends React.Component {
     state = {
         resultsFound: 0,
         order: 0,
         results: [],
         user: "",
+        invitees: [],
+        input: "",
+        confirmed: 0,
     }
 
     componentDidMount() {
-      this.setState({
-        order: this.props.match.url.split('/')[2],
-        user: JSON.parse(localStorage.getItem('user')) || null,
-      })
+                this.setState({
+                    order: this.props.match.url.split('/')[2],
+                    user: JSON.parse(localStorage.getItem('user')) || null,
+                })
+    }
 
+    handleOnChange = (e) => {
+        this.setState({ input: e.target.value })
+    }
 
+    handleAdd = (e) => {
+        e.preventDefault();
+        this.setState({
+            invitees: this.state.invitees.concat(this.state.input),
+            input: ""
+        })
+    }
+  
+    handleSubmit = (e) => {
+        this.state.invitees.forEach((e,i)=>{
+            createOrderRequest(this.state.order, e)
+            .then(()=>{
+                this.setState({confirmed: this.state.confirmed++})
+            })
+            .then(()=>{
+                if(this.state.counter === 5){
+                    this.props.history.push('/dashboard')
+                }
+            })
+        })
     }
 
 
-    render () {
-        return(
+    render() {
+        return (
             <div className="container">
-            {
-             this.state.user !== null ? <>
-             <div className="row my-4">Results: {this.state.resultsFound}</div>
-             <div className="row">
-               <div className="col card">
-               {
-                 this.state.results.length > 0 ? this.state.results.map((e,i)=>{
-                   return <>
-                     <Link to={`/menu/${e.restaurant.R.res_id}`} style={{textDecoration: "none", color: "black"}}><div className="row card-body" key={i} >
-                       <div className="col-3">
-                         <img src={e.restaurant.featured_image} alt={e.restaurant.name} style={{height: "100px"}}/>
-                       </div>
-                       <div className="col-3">
-                         <p>{e.restaurant.name}</p>
-                         <p> Cusine {e.restaurant.cuisines}</p>
-                       </div>
-                       <div className="col-3">
-                         <p> Rated {e.restaurant.user_rating.aggregate_rating}/5</p>
-                         <p> {e.restaurant.user_rating.votes} Votes</p>
-                       </div>
-                       <div className="col-3">
-                       <p>{e.restaurant.location.address}</p>
-                       <p>{e.restaurant.location.locality}</p>
-                       <p>{e.restaurant.location.city}, {e.restaurant.location.zipcode}</p>
-                       </div>
-                     </div>
-                     </Link>
-                   </>
-                 }) : null
-               }
-               </div>
-             </div>
-             </> : <Redirect to='/'/> 
-            }
+                {
+                    this.state.user === null ?
+                        <form>
+                            <div class="form-group">
+                            <div className="row">
+                            <div className="col-10">
+                                <label for="exampleInputEmail1">Email address</label>
+                                <input type="email" value={this.state.input} class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.handleOnChange}/>
+                                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                            </div>
+                            <div  className="col-2" style={{margin: "auto 0"}}>
+                            <button type="submit" class="btn btn-primary" onClick={this.handleAdd}>Add</button>
+                            </div>
+                            </div>
+                            <button type="submit" class="btn btn-primary mt-3">Submit</button>
+                            </div>
+                        </form> 
+                    : null//<Redirect to='/' />
+                }
+                {
+                    this.state.invitees.length>0 ? this.state.invitees.map((e,i)=>{
+                        return <div className="row">
+                        <div className="col mx-5 my-2">
+                            <p> Invitee {i+1} : {e}</p>
+                        </div>
+                        </div>
+                    }) : null
+                }
             </div>
         )
     }
 }
 
-export default RestaurantSearchResults;
+export default InviteUsers;
