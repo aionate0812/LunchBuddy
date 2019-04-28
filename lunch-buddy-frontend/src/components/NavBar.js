781917968733de6da521ee0cbde89e2cb8fd34ee
ositaigwe@pursuit.org <j0h@example.com>
2019-04-28 15:16:16 -0400

    styling

--- lunch-buddy-frontend/src/components/NavBar.js
+++ lunch-buddy-frontend/src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = props => {
             <nav className="row navbar navbar-expand-lg navbar-light" style={{backgroundColor: "maroon"}}>
             <div className="col"></div>
             <div className="col-8">
-                <Link className="navbar-brand" to='/'><img src={logo} alt="Lunch Buddy"/></Link>
+                <Link className="navbar-brand" to='/'><img src="https://pngimage.net/wp-content/uploads/2018/06/friends-icon-png-white-1.png" style={{width: "10%"}}/><img src={logo} alt="Lunch Buddy"/></Link>
             </div>
             <div className="col-3">
                 <div className="collapse navbar-collapse" id="navbarNav">
