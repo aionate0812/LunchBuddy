124a5678c5cd4e11ba34a7b4b21d81adbd5beabb
AaronTeele <aaronteele@AaronTeeles-MacBook-Air.local>
2019-04-28 16:46:44 -0400

    finalized logout feature

--- lunch-buddy-frontend/src/components/NavBar.js
+++ lunch-buddy-frontend/src/components/NavBar.js
@@ -1,31 +1,50 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/logo.png';
 
-const NavBar = props => {
-    return (
-        <>
-            <nav className="row navbar navbar-expand-lg navbar-light" style={{backgroundColor: "maroon"}}>
-            <div className="col"></div>
-            <div className="col-8">
-                <Link className="navbar-brand" to='/'><img src={logo} alt="Lunch Buddy"/></Link>
-            </div>
-            <div className="col-3">
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item active" >
-                            <Link className="nav-link" to="/" style={{color: "white"}}> Home </Link>
-                        </li>
-                        <li className="nav-item active">
-                            <Link className="nav-link" to="/order" style={{color: "white"}}> Create Order </Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-            </nav>
+class NavBar extends Component {
 
-        </>
-    )
+    logOut = () => {
+        const user = localStorage.getItem('user')
+        if (user) return "Log Out"
+        return ""
+    }
+
+    handleClick = e => {
+        e.preventDefault();
+        localStorage.clear()
+        this.props.history.push('/')
+    }
+
+    render() {
+        return (
+            <>
+                <nav className="row navbar navbar-expand-lg navbar-light" style={{ backgroundColor: "maroon" }}>
+                    <div className="col"></div>
+                    <div className="col-8">
+                        <Link className="navbar-brand" to='/'><img src={logo} alt="Lunch Buddy" /></Link>
+                    </div>
+                    <div className="col-3">
+                        <div className="collapse navbar-collapse" id="navbarNav">
+                            <ul className="navbar-nav">
+                                <li className="nav-item active" >
+                                    <Link className="nav-link" to="/" style={{ color: "white" }}> Home </Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="/order" style={{ color: "white" }}> Create Order </Link>
+                                </li>
+                                <li className="nav-item active">
+                                    <Link className="nav-link" to="/" style={{ color: "white" }} onClick={this.handleClick}>{this.logOut()}</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+            </>
+        )
+    }
+
 }
 
 export default NavBar
