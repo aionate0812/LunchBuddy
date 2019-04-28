09b9c7c3d290b22665b0e02fd333b7f18c20b98e
ositaigwe@pursuit.org <j0h@example.com>
2019-04-27 20:30:49 -0400

    changes backend index.js

--- lunch-buddy-backend/services/orders.js
+++ lunch-buddy-backend/services/orders.js
@@ -3,12 +3,12 @@ const db = require('./dbConnect')
 const ordersService = {}
 
 
-ordersService.createOrder = (order_id) => {
-    return db.one('SELECT * FROM orders WHERE orders.id=$[order.id]', {order_id})
+ordersService.readOrder = (order_id) => {
+    return db.any('SELECT * FROM orders WHERE orders.id=$[order.id]', {order_id})
 }
 
 ordersService.createOrder = (user_id) => {
     return db.one('INSERT INTO orders (user_id) VALUES (${user_id}) RETURNING id', {user_id})
 }
 
-module.exports = orderService
+module.exports = ordersService
