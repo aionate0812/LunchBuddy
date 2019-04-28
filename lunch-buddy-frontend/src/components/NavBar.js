144d76da3a66d2433e8ffd3100157119819f9a98
ositaigwe@pursuit.org <j0h@example.com>
2019-04-28 14:55:34 -0400

    styling

--- lunch-buddy-frontend/src/components/NavBar.js
+++ lunch-buddy-frontend/src/components/NavBar.js
@@ -6,10 +6,11 @@ const NavBar = props => {
     return (
         <>
             <nav className="row navbar navbar-expand-lg navbar-light" style={{backgroundColor: "maroon"}}>
+            <div className="col"></div>
             <div className="col-8">
                 <Link className="navbar-brand" to='/'><img src={logo} alt="Lunch Buddy"/></Link>
             </div>
-            <div className="col-4">
+            <div className="col-3">
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item active" >
--- lunch-buddy-frontend/src/containers/UserLogin.js
+++ lunch-buddy-frontend/src/containers/UserLogin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import axios from 'axios'
-import {background} from '../assets/healthy-lunch.jpg'
+import background from '../assets/healthy-lunch.jpg'
 
 class UserLogin extends React.Component {
     state = {
@@ -50,20 +50,22 @@ class UserLogin extends React.Component {
             <>
                 <br />
                 <div className="col-xs-12 py-2" style={{ height: "40px", textAlign: "center", backgroundColor: "black", color: "white"}}>Please Login to Find Your Lunch Buddy</div>
-                <div className='container my-auto'>
+                <div className='container my-auto' style={{fontFamily: "Arvo"}}>
                     <div className='row'>
                     {
                      this.state.error   
                     }
                         <form style={{ 'width': '100%' }} >
                             <div className="input-group flex-nowrap my-3">
-
+                            <div className="col-8">
                                 <input type="text" name='input' className="form-control" placeholder="Enter Username or Email Here" aria-label="Username or Email" aria-describedby="addon-wrapping" onChange={this.handleChange} />
                             </div>
-                            <div style={{ 'textAlign': 'center' }}>
-                                <button type="button" className="btn btn-primary btn-md" onClick={this.handleSubmit}>Login</button>
+                            <div className="col-4" style={{ 'textAlign': 'center' }}>
+                                <button type="button" className="btn btn-success btn-lg" onClick={this.handleSubmit}>Login</button>
+                            </div>
                             </div>
                         </form>
+                        <img src={background} alt="healthy lunch" style={{width: "100%"}}/>
                     </div>
                 </div>
             </>
